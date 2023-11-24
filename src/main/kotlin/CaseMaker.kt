7fcee989ef9a4650385b90a6d27171ac19eb8d07
YS <etgt777@naver.com>
2023-11-24 23:39:12 +0900

    case make sucsses

---
+++ src/main/kotlin/CaseMaker.kt
@@ -0,0 +1,36 @@
+fun caseMake(caseNumber: MutableList<Double>): MutableList<MutableList<Double>>{
+    var makeAllCase = mutableListOf<MutableList<Double>>()
+    var nowCase = mutableListOf<Double>()
+
+
+    caseNumber.sort()
+
+    for (i in 0 until caseNumber.size){
+        for (j in 0 until caseNumber.size) {
+            if (j != i) {
+                for (k in 0 until caseNumber.size) {
+                    if (k != i && k != j) {
+                        for (l in 0 until caseNumber.size ) {
+                            if (l != i && l != j && l != k) {
+                                for (m in 0 until caseNumber.size) {
+                                    if(m != i && m != j && m != k && m != l) {
+                                        nowCase += caseNumber[i]
+                                        nowCase += caseNumber[j]
+                                        nowCase += caseNumber[k]
+                                        nowCase += caseNumber[l]
+                                        nowCase += caseNumber[m]
+                                        makeAllCase += nowCase.toMutableList()
+                                        nowCase.clear()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return makeAllCase.toMutableList()
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,43 +2,13 @@
 
 fun main() {
 
-    var caseNumber = mutableListOf<Double>()
-    var allCase = mutableListOf<MutableList<Double>>()
-    var nowCase = mutableListOf<Double>()
-    var range = 0 until caseNumber.size
+    var number = mutableListOf<Double>(10.0, 15.0, 5.0, 8.0, 7.0)
 
-    while (caseNumber.size < 5) {
-        caseNumber += readLine()!!.toDouble()
-    }
+    val allCase = caseMake(number.toMutableList())
 
-    caseNumber.sort()
 
-    for (i in range){
-        for (j in range) {
-            if (j != i) {
-                for (k in range) {
-                    if (k != i && k != j) {
-                        for (l in range ) {
-                            if (l != i && l != j && l != k) {
-                                for (m in range) {
-                                    if(m != i && m != j && m != k && m != l) {
-                                        nowCase += caseNumber[i]
-                                        nowCase += caseNumber[j]
-                                        nowCase += caseNumber[k]
-                                        nowCase += caseNumber[l]
-                                        nowCase += caseNumber[m]
-                                        allCase += nowCase.toMutableList()
-                                        nowCase.clear()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     println("all case : ${allCase.joinToString()}")
+    println("all case size : ${allCase.size}")
 
 }
